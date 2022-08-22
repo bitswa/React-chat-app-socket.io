1c441787917bce51ef1f77a887441e5d6a9b2da2
bitswa <bitswa@pm.me>
2022-08-22 02:13:12 -0300

    disconnect prob fixed

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
   },
 });
 
-const users = [];
+let users = [];
 
 io.on("connection", (socket) => {
   console.log(socket.id);
@@ -25,45 +25,44 @@ io.on("connection", (socket) => {
       "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
     username: "",
   });
-  
+
   io.emit("connected", { users });
 
   socket.on("send_message", ({ to, from, content }) => {
-    console.log(to, from, content);
     socket.to(to).emit("private_message", {
       from,
       content,
     });
   });
 
-  socket.on("message_state", ({to, state}) => {
-    console.log(to, state)
+  socket.on("message_state", ({ to, state }) => {
     socket.to(to).emit("receive_message_state", {
-      state
-    })
-  })
+      state,
+    });
+  });
 
   socket.on("user_modification", ({ userId, image, username }) => {
-    console.log(users);
     users.map((user, index) => {
       if (user.userId !== userId) return;
 
       users.splice(index, 1);
-      console.log(userId, image, username);
+
       users.push({
         userId,
         image,
         username,
       });
-      console.log(users);
 
       io.emit("connected", { users });
     });
   });
 
   socket.on("disconnect", () => {
-    const newUsers = users.map((user) => user.userId.indexOf(socket.id));
-    users.splice(newUsers, 1);
+    // const newUsers = users.map((user) => user.userId.indexOf(socket.id));
+    // users.splice(newUsers, 1);
+    users = users.filter((user) => user.userId !== socket.id);
+    console.log(socket.id, "desconectado");
+    // console.log(users);
     io.emit("connected", { users });
   });
 });
